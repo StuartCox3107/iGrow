958c04f7440051b167a8457e2c66dc1414dfeaf7
StuartCox3107 <coxstuart@hotmail.com>
2020-10-10 12:33:25 +0000

    js moved from body.html into own file & folder

---
+++ static/js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+            $('.datepicker').datepicker()
+            $('.collapsible').collapsible();
+            $('select').material_select();
+            $(".button-collapse").sideNav();
+            $('input#input_text, textarea#textarea2').characterCounter();
+        })
+        
+      $(".datepicker").datepicker({
+        selectMonths: true, 
+        selectYears: 10, 
+        today: "Today",
+        clear: "Clear",
+        close: "Ok",
+        format: 'dd/mm/yyyy',
+        closeOnSelect: false, 
+      });
+
+      document.getElementById("matfix").addEventListener("click", function(e) {
+	e.stopPropagation();
+});

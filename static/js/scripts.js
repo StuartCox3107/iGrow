68425607ca068e37a6f2e9ed8f379e74545a9e80
StuartCox3107 <coxstuart@hotmail.com>
2020-10-18 18:35:39 +0000

    heroku push

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,20 +1,16 @@
-$(document).ready(function() {
-            $('.collapsible').collapsible();
-            $(".button-collapse").sideNav();
-            $('input#input_text, textarea#textarea2').characterCounter();
-        })
-        
-      $(".datepicker").pickadate({
-        selectMonths: true, 
-        selectYears: 10, 
-        today: "Today",
-        clear: "Clear",
-        close: "Ok",
-        format: 'dd/mm/yyyy',
-        closeOnSelect: true, 
-      });
-
-      document.getElementById("matfix").addEventListener("click", function(e) {
-    e.stopPropagation();
-    
+$(document).ready(function () {
+  $(".collapsible").collapsible();
+  $(".sidenav").sidenav();
+  $("input#input_text, textarea#textarea2").characterCounter();
 });
+
+$('.datepicker').datepicker({
+        format: "dd, mmmm yyyy",
+        yearRange: 10,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
+
+
